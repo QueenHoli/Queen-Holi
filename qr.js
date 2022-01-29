29d5f2172f3b41c2884fe9991fbfa7d17959fc3b
QueenHoli <98634055+QueenHoli@users.noreply.github.com>
2022-01-29 13:52:27 +0530

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,64 @@
+/* Copyright (C) 2021 Queen Holi
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Queen-Holi
+*/
+
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+async function whatsAsena() {
+  const conn = new WAConnection();
+  conn.logger.level = 'warn';
+  conn.version = [2, 2126, 14]
+
+  conn.on('connecting', async () => {
+    console.log(`${chalk.green.bold('Amazone Alexa')}${chalk.green.bold('Team')}
+${chalk.white.italic('amazone String code recipient')}
+${chalk.blue.bold('⛔️ Connecting to whatsapp, please wait.....')}`);
+  });
+
+  conn.on('open', async () => {
+    console.log(
+      chalk.green.bold('Holi QR Code: '),
+      'QUEEN HOLI;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      )
+    );
+    await conn.sendMessage(
+      conn.user.jid,
+      'QUEEN HOLI;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      ),
+      MessageType.text
+    );
+    if (conn.user.jid.startsWith('91')) {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ Successfully scanned♥️️*\n*▪️Thanks For using Queen Holi*',
+        MessageType.text
+      );
+    } else {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ Successfully scanned♥️️️*\n*▪️Thanks For using Queen Holi*',
+        MessageType.text
+      );
+    }
+    console.log(
+      chalk.green.bold(
+        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
+      ),
+      chalk.green.bold(
+        'If You Cant copy this code, please check your whatsapp log number✅️'
+      )
+    );
+    process.exit(0);
+  });
+
+  await conn.connect();
+}
+
+whatsAsena();
